245a80ff893bf934c30d062c749409118373e019
aarav-gargg <aaravgarg975@gmail.com>
2024-07-12 23:39:23 +0530

    Domicile Added in FormPage and Navbar recreated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './Components/navbar/Navbar';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import FormPage from './Components/InputForm/FormPage';
+import Layout from './Layout';
+// import Login from './Components/Login';
+import Contact from './Components/contact/Contact';
+import Home from './Components/home/Home';
+import Articles from './Components/articles/Articles';
 
-import './App.css'
-import Navbar from './Components/navbar/Navbar'
-import FormPage from './Components/InputForm/FormPage'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='Home' element={<Home/>} />
+      <Route path='CollegePredictor' element={<FormPage />} />
+      <Route path='Contact' element={<Contact/>} />
+      <Route path='Articles' element={<Articles/>}/>
+      {/* <Route path='Login' element={<Login />} /> */}
+    </Route>
+  )
+);
 
 function App() {
   return (
     <>
-      <FormPage/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/Components/InputForm/FormPage.jsx
+++ src/Components/InputForm/FormPage.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
 import "./FormPage.css"
 import InputLabel from '@mui/material/InputLabel';
+import { useTheme } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import Chip from '@mui/material/Chip';
+import OutlinedInput from '@mui/material/OutlinedInput';
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 
 function FormPage() {
+  const names = [
+    "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+    "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka",
+    "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram",
+    "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana",
+    "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman and Nicobar Islands",
+    "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu", "Delhi", "Lakshadweep",
+    "Puducherry", "Ladakh", "Jammu and Kashmir"
+  ];
+  
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
   const [course, setcourse] = React.useState('');
 
   const handlecourse = (event) => {
@@ -84,6 +129,26 @@ function FormPage() {
           <MenuItem value={"male"}>Male</MenuItem>
           <MenuItem value={"Female"}>Female</MenuItem>
           <MenuItem value={"o"}>Others</MenuItem>
+        </Select>
+        <InputLabel id="demo-multiple-name-label">Domicile State</InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="Name" />}
+          MenuProps={MenuProps}
+          className='domicile'
+        >
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
         </Select>
       </div>
       </div>
