14fbddce5b88ad3752df2b1a80a1dd1a599cbc71
aarav-gargg <aaravgarg975@gmail.com>
2024-08-09 22:13:43 +0530

    updated the colleges page

--- FrontEnd/src/Components/Team/Team.jsx
+++ FrontEnd/src/Components/Team/Team.jsx
@@ -1,119 +1,116 @@
-import React from 'react'
-import './Team.css'
+import React from 'react';
+import './Team.css';
 import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import { Navigate } from 'react-router-dom';
-
-
 
 const Team = () => {
     return (
         <>
-            <div className="responsive-container-block outer-container">
-                <div className="responsive-container-block inner-container">
-                    <p className="text-blk section-head-text">
+            <div className="team-responsive-container-block team-outer-container">
+                <div className="team-responsive-container-block team-inner-container">
+                    <p className="team-text-blk team-section-head-text">
                         Our Team
                     </p>
-                    <p className="text-blk section-subhead-text">
+                    <p className="team-text-blk team-section-subhead-text">
                         Meet the Visionaries Crafting Tomorrow: The Dynamic Team Behind Our Innovations
                     </p>
-                    <div className="responsive-container-block">
-                        <div className="swiper team-swiper">
-                            <div className="swiper-wrapper">
-                                <div className="swiper-slide">
-                                    <div className="card c-color" data-aos="zoom-in"  data-aos-duration="1000">
-                                        <div className="img-wrapper">
-                                            <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw40.png" />
+                    <div className="team-responsive-container-block">
+                        <div className="team-swiper">
+                            <div className="team-swiper-wrapper">
+                                <div className="team-swiper-slide">
+                                    <div className="team-card team-c-color" data-aos="zoom-in" data-aos-duration="1000">
+                                        <div className="team-img-wrapper">
+                                            <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw40.png" alt="Aarav Garg" />
                                         </div>
-                                        <div className="card-content">
-                                            <p className="text-blk name">
+                                        <div className="team-card-content">
+                                            <p className="team-text-blk team-name">
                                                 Aarav Garg
                                             </p>
-                                            <p className="text-blk position">
+                                            <p className="team-text-blk team-position">
                                                 Full Stack Developer
                                             </p>
-                                            <p className="text-blk testimonial">
+                                            <p className="team-text-blk team-testimonial">
                                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                                 eiusmod tempor incididunt ut labore et dolore magna
                                             </p>
                                         </div>
-                                        <div className="icon-buttons">
-                                        <IconButton aria-label="delete">
-                                                <a href='https://www.linkedin.com/in/aarav-garg-5b1715273' target='_blank'><LinkedInIcon /></a>
+                                        <div className="team-icon-buttons">
+                                            <IconButton aria-label="linkedin">
+                                                <a href='https://www.linkedin.com/in/aarav-garg-5b1715273' target='_blank' rel="noreferrer"><LinkedInIcon /></a>
                                             </IconButton>
-                                            <IconButton aria-label="delete" >
-                                               <a href='https://github.com/aarav-gargg' target='_blank'> <GitHubIcon /></a>
+                                            <IconButton aria-label="github">
+                                                <a href='https://github.com/aarav-gargg' target='_blank' rel="noreferrer"><GitHubIcon /></a>
                                             </IconButton>
-                                            <IconButton color="secondary" aria-label="add an alarm">
+                                            <IconButton color="secondary" aria-label="pdf">
                                                 <PictureAsPdfIcon />
                                             </IconButton>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="swiper-slide">
-                                    <div className="card c-color" data-aos="zoom-in"  data-aos-duration="1000">
-                                        <div className="img-wrapper">
-                                            <img src="/pic2.jpg" />
+                                <div className="team-swiper-slide">
+                                    <div className="team-card team-c-color" data-aos="zoom-in" data-aos-duration="1000">
+                                        <div className="team-img-wrapper">
+                                            <img src="/pic2.jpg" alt="Amitoj Singh Bakshi" />
                                         </div>
-                                        <div className="card-content">
-                                            <p className="text-blk name">
+                                        <div className="team-card-content">
+                                            <p className="team-text-blk team-name">
                                                 Amitoj Singh Bakshi
                                             </p>
-                                            <p className="text-blk position">
+                                            <p className="team-text-blk team-position">
                                                 Full Stack Developer
                                             </p>
-                                            <p className="text-blk testimonial">
+                                            <p className="team-text-blk team-testimonial">
                                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                                 eiusmod tempor incididunt ut labore et dolore magna
                                             </p>
                                         </div>
-                                        <div className="icon-buttons">
-                                        <IconButton aria-label="delete">
-                                                <a href='https://www.linkedin.com/in/amitoj-singh-51a058247' target='_blank'><LinkedInIcon /></a>
+                                        <div className="team-icon-buttons">
+                                            <IconButton aria-label="linkedin">
+                                                <a href='https://www.linkedin.com/in/amitoj-singh-51a058247' target='_blank' rel="noreferrer"><LinkedInIcon /></a>
                                             </IconButton>
-                                            <IconButton aria-label="delete" >
-                                               <a href='https://github.com/amitoj0605' target='_blank'> <GitHubIcon /></a>
+                                            <IconButton aria-label="github">
+                                                <a href='https://github.com/amitoj0605' target='_blank' rel="noreferrer"><GitHubIcon /></a>
                                             </IconButton>
-                                            <IconButton color="secondary" aria-label="add an alarm">
+                                            <IconButton color="secondary" aria-label="pdf">
                                                 <PictureAsPdfIcon />
                                             </IconButton>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="swiper-slide">
-                                    <div className="card c-color" data-aos="zoom-in"  data-aos-duration="1000">
-                                        <div className="img-wrapper">
-                                            <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw42.png" />
+                                <div className="team-swiper-slide">
+                                    <div className="team-card team-c-color" data-aos="zoom-in" data-aos-duration="1000">
+                                        <div className="team-img-wrapper">
+                                            <img src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw42.png" alt="Chinmay Singh" />
                                         </div>
-                                        <div className="card-content">
-                                            <p className="text-blk name">
+                                        <div className="team-card-content">
+                                            <p className="team-text-blk team-name">
                                                 Chinmay Singh
                                             </p>
-                                            <p className="text-blk position">
+                                            <p className="team-text-blk team-position">
                                                 Full Stack Developer
                                             </p>
-                                            <p className="text-blk testimonial">
+                                            <p className="team-text-blk team-testimonial">
                                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                                 eiusmod tempor incididunt ut labore et dolore magna
                                             </p>
                                         </div>
-                                        <div className="icon-buttons">
-                                            <IconButton aria-label="delete">
-                                                <a href='https://www.linkedin.com/in/chinmay-singh-4092b0253' target='_blank'><LinkedInIcon /></a>
+                                        <div className="team-icon-buttons">
+                                            <IconButton aria-label="linkedin">
+                                                <a href='https://www.linkedin.com/in/chinmay-singh-4092b0253' target='_blank' rel="noreferrer"><LinkedInIcon /></a>
                                             </IconButton>
-                                            <IconButton aria-label="delete" >
-                                               <a href='https://github.com/Chinmay67' target='_blank'> <GitHubIcon /></a>
+                                            <IconButton aria-label="github">
+                                                <a href='https://github.com/Chinmay67' target='_blank' rel="noreferrer"><GitHubIcon /></a>
                                             </IconButton>
-                                            <IconButton color="secondary" aria-label="add an alarm">
+                                            <IconButton color="secondary" aria-label="pdf">
                                                 <PictureAsPdfIcon />
                                             </IconButton>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="swiper-pagination container-block">
+                            <div className="team-swiper-pagination team-container-block">
                             </div>
                         </div>
                     </div>
@@ -123,4 +120,4 @@ const Team = () => {
     )
 }
 
-export default Team
+export default Team;
--- FrontEnd/src/Components/colleges/CollegeCard.jsx
+++ FrontEnd/src/Components/colleges/CollegeCard.jsx
@@ -1,180 +1,70 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./CollegeCard.css";
-
+import data from "../collegePreview/data.json";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Link } from 'react-router-dom';
+import { Navigation, Pagination } from 'swiper/modules';
+import { useNavigate } from 'react-router-dom';
 
 const ArticleCard = () => {
-    useEffect(() => {
-        const nextDom = document.getElementById('next');
-        const prevDom = document.getElementById('prev');
-    
-        const carouselDom = document.querySelector('.carousel');
-        const SliderDom = carouselDom.querySelector('.carousel .list');
-        const thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-        const thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-        const timeDom = document.querySelector('.carousel .time');
-    
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        const timeRunning = 3000;
-        const timeAutoNext = 7000;
-    
-        nextDom.onclick = function() {
-          showSlider('next');    
-        }
-    
-        prevDom.onclick = function() {
-          showSlider('prev');    
-        }
-    
-        let runTimeOut;
-        let runNextAuto = setTimeout(() => {
-          nextDom.click();
-        }, timeAutoNext);
-    
-        function showSlider(type) {
-          const SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-          const thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-          if (type === 'next') {
-            SliderDom.appendChild(SliderItemsDom[0]);
-            thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-            carouselDom.classList.add('next');
-          } else {
-            SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-            thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-            carouselDom.classList.add('prev');
-          }
-    
-          clearTimeout(runTimeOut);
-          runTimeOut = setTimeout(() => {
-            carouselDom.classList.remove('next');
-            carouselDom.classList.remove('prev');
-          }, timeRunning);
-    
-          clearTimeout(runNextAuto);
-          runNextAuto = setTimeout(() => {
-            nextDom.click();
-          }, timeAutoNext);
-        }
-      }, []);
-  return (
-    <div>
- <div className="carousel">
-        <div className="list">
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="author">UniFind</div>
-                    <div className="title">DESIGN SLIDER</div>
-                    <div className="topic">ANIMAL</div>
-                    <div className="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div className="buttons">
-                        <button>READ MORE</button>
+    //   const navigate = useNavigate();
 
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="author">LUNDEV</div>
-                    <div className="title">DESIGN SLIDER</div>
-                    <div className="topic">ANIMAL</div>
-                    <div className="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div className="buttons">
-                        <button>READ MORE</button>
-                       
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="author">LUNDEV</div>
-                    <div className="title">DESIGN SLIDER</div>
-                    <div className="topic">ANIMAL</div>
-                    <div className="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div className="buttons">
-                        <button>READ MORE</button>
-                      
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="author">LUNDEV</div>
-                    <div className="title">DESIGN SLIDER</div>
-                    <div className="topic">ANIMAL</div>
-                    <div className="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div className="buttons">
-                        <button>READ MORE</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="thumbnail">
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src="public/projectPic.png"/>
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-        </div>
+    //   const handleReadMore = (collegeId) => {
+    //     navigate(`/college/${collegeId}`);
+    //   };
 
-        <div className="arrows">
-            <button id="prev">{'<'}</button>
-            <button id="next">{'>'}</button>
+    return (
+        //     <div className="college-card-container">
+        //       <Swiper
+        //         spaceBetween={50}
+        //         slidesPerView={1}
+        //         navigation
+        //         pagination={{ clickable: true }}
+        //         modules={[Navigation, Pagination]}
+        //       >
+        //         {data.map((college, index) => (
+        //           <SwiperSlide
+        //             key={index}
+        //             style={{ backgroundImage: `url(${college.imageUrl})` }}
+        //           >
+        //             <div className="college-card">
+        //               <div className="college-name">{college.name}</div>
+        //               <div className="college-description">
+        //                 {college.description}
+        //               </div>
+        //               <button
+        //                 className="read-more-btn"
+        //                 onClick={() => handleReadMore(college.id)}
+        //               >
+        //                 Read More
+        //               </button>
+        //             </div>
+        //           </SwiperSlide>
+        //         ))}
+        //       </Swiper>
+        //     </div>
+        //   );
+        <div className="CollgesList">
+           {data.map((college,index)=>(
+             <div key={index} className="collegeArticle" data-aos="fade-up-left" data-aos-duration="1000">
+             <div className="collegeArticleImg">
+                 <img src={college.imageUrl} alt="" />
+             </div>
+             <div class="collegeArticleContent">
+                 <Link to={`/College/${college.id}`}>
+                     <h3>{college.name}</h3>
+                 </Link>
+                 <p>{college.preview}</p>
+                 <button><Link to={`/College/${college.id}`}>
+                     <h3>Read More</h3>
+                 </Link></button>
+             </div>
+         </div>
+           ))}
         </div>
-        <div className="time"></div>
-    </div>
-    </div>
-  );
-}
+    );
+};
 
 export default ArticleCard;
