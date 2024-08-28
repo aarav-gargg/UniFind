d99429472e4240acb6876602e46b37c9b6f72bc6
aarav-gargg <aaravgarg975@gmail.com>
2024-08-28 23:07:47 +0530

    "Update Axios base URL from localhost to https://unifind-e66m.onrender.com"

--- FrontEnd/src/api/axios.js
+++ FrontEnd/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://localhost:8000', // Your API base URL
+    baseURL: 'https://unifind-e66m.onrender.com', // Your API base URL
     withCredentials: true, // Ensure cookies are sent with requests
 });
 export default instance;
